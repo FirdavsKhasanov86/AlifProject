98c1b6bce5f444cf8c5438a8a17f3da132d99c3f
user <user@Williams-iMac.local>
2020-04-23 14:23:11 +0500

    AlifProject

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace AlifProject
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+        }            
+
+            
+        
+     
+    }
+}
+    
+
