f811ff3c595ec96746cb642edf9d29cb74b1ddfe
user <user@Williams-iMac.local>
2020-04-23 14:28:29 +0500

    Added connection to DB

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace AlifProject
         static void Main(string[] args)
         {
            
+            string conString = @"Data source=192.168.103.137; Initial catalog=AlifProject; user id=sa; password=Sa123.";
+            SqlConnection con = new SqlConnection(conString);
+            Console.Clear();
+            System.Console.WriteLine("Выберите свою роль Клиент/Администратор:");
+            System.Console.WriteLine("Роль Администратор нажмите - 1");
+            System.Console.WriteLine("Роль Клиент нажмите - 2");
+
+         
+           
+
+          
         }            
 
             
